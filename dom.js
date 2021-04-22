772d5f4bb4d3788ceefac137c68cc5113ea834f0
BronzwikGk <67652347+bronzwikgk@users.noreply.github.com>
2021-04-23 02:40:53 +0530

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,14 @@
+
+
+class test{
+
+  constructor(){
+  }
+  
+  test(){
+window.open(path.html,'_blank', 'toolbar=no,status=no,menubar=no,scrollbars=no,resizable=no,left=10000, top=10000, width=10, height=10, visible=none', '');  
+  }
+  
+  
+}
+  
